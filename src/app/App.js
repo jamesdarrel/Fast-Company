6cb5f57242688156a1bae21e3688be3b7308df54
Mykhaylo Misyuk <jamesdarrel@MacBook-Air-Mykhaylo.local>
2022-02-21 15:08:42 +0200

    lesson 14 done

--- src/app/App.js
+++ src/app/App.js
@@ -6,6 +6,7 @@ import Login from "./layouts/login";
 import Main from "./layouts/main";
 import NavBar from "./components/ui/navBar";
 import { ProfessionProvider } from "./hooks/useProfession";
+import { QualitiesProvider } from "./hooks/useQuality";
 
 function App() {
     return (
@@ -13,8 +14,13 @@ function App() {
             <NavBar />
             <Switch>
                 <ProfessionProvider>
-                    <Route path="/users/:userId?/:edit?" component={Users} />
-                    <Route path="/login/:type?" component={Login} />
+                    <QualitiesProvider>
+                        <Route
+                            path="/users/:userId?/:edit?"
+                            component={Users}
+                        />
+                        <Route path="/login/:type?" component={Login} />
+                    </QualitiesProvider>
                 </ProfessionProvider>
                 <Route path="/" exact component={Main} />
                 <Redirect to="/" />
--- src/app/components/ui/qualities/quality.jsx
+++ src/app/components/ui/qualities/quality.jsx
@@ -1,16 +1,21 @@
 import React from "react";
+import { useQualities } from "../../../hooks/useQuality";
 import PropTypes from "prop-types";
-const Quality = ({ color, name, _id }) => {
+
+const Quality = ({ ids }) => {
+    const { getQuality } = useQualities();
+    const qual = getQuality(ids);
+
     return (
-        <span className={"badge m-1 bg-" + color} key={_id}>
-            {name}
+        <span className={"badge m-1 bg-" + qual.color} key={qual._id}>
+            {qual.name}
         </span>
     );
 };
 Quality.propTypes = {
-    color: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    _id: PropTypes.string.isRequired
+    // color: PropTypes.string.isRequired,
+    // name: PropTypes.string.isRequired,
+    ids: PropTypes.oneOfType([PropTypes.string, PropTypes.array])
 };
 
 export default Quality;
